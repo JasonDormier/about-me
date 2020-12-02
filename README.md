3ffa79a19e0c55742e2beb42587b864694851ba3
JasonDormier <jmdormier@gmail.com>
2020-12-01 16:41:02 -0800

    Completion of lab 2

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # about-me
+
+## About me
+
+Built an About-me quiz that asks the end user questions for them to answer about me.
+
+### Author: Jason Dormier
+
+### Links and Resources
+
+Referenced some of my work from Udemy Javascript course.
+
+### Reflections and comments
+
+This was fun and I decided to go a step further with my js code and add some funtions for the if statements and respones.
--- js/app.js
+++ js/app.js
@@ -1,27 +1,50 @@
-"use strict";
+'use strict';
 
-let party = "";
+let answer = '';
 
-const userName = prompt("What is your name?").toLowerCase();
+let userName = '', q1 = '', q2 = '', q3 = '', q4 = '';
+
+const responseYes = function (input) {
+  if (input === 'yes' || input === 'y') {
+    answer = 'correct! Nice work!';
+  } else {
+    answer = 'Incorrect, nice try';
+  }
+  alert(answer);
+};
+
+const responseNo = function (input) {
+  if (input === 'no' || input === 'n') {
+    answer = 'correct! Nice work!';
+  } else {
+    answer = 'Incorrect, nice try';
+  }
+  alert(answer);
+};
+
+userName = prompt('What is your name?').toLowerCase();
 //console.log(`The users name is ${userName}.`);
 
-const userLocation = prompt("Where do you live?").toLowerCase();
-//console.log(`The users name is ${userLocation}.`);
+q1 = prompt(`Hello ${userName}, welcome to my interactive bio. Is my name Jason?`).toLowerCase();
+console.log(q1);
+responseYes(q1);
 
-const userAge = prompt(`How old are you ${userName}?`).toLowerCase();
-//console.log(`${userAge}.`);
+q2 = prompt('Are the Seahawks my favorite football team?').toLowerCase();
+console.log(q2);
+responseYes(q2);
 
-let userParty = prompt("Do you like to party? yes or no?").toLowerCase();
-//console.log(`${userParty}`);
+q3 = prompt('Is soccer my favorite sport?').toLowerCase();
+console.log(q3);
+responseNo(q3);
 
-if (userParty === "yes" || userParty === "y") {
-  party = `Rightous, Party on! `;
-} else {
-  party = `No worries, there are plenty of other things to do beside party.`;
-}
+q4 = prompt('Am I in my 40s?').toLowerCase();
+console.log(q4);
+responseNo(q4);
 
-alert(
-  `Well hello ${userName}, ${userAge} that's a good age, and ${userLocation} is a nice place to live. ${party} `
-);
+alert(`Well ${userName}, thanks for playing. See ya!`);
 
-var poptart = 24
+
+
+
+
+//alert(`Well hello ${userName}, ${userAge} that's a good age, and ${userLocation} is a nice place to live. ${party}`);
